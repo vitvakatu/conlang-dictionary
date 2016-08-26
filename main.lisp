60b2292b883e7a4671fdc79d0592c3727d139eb8
Ilya Bogdanov <fumlead@gmail.com>
2016-08-26 11:24:30 +0300

    Simple 'Hello, Qt'

---
+++ main.lisp
@@ -0,0 +1,17 @@
+(defpackage :conlang-dict
+  (:use :cl :qt)
+  (:export :main))
+(in-package :conlang-dict)
+(named-readtables:in-readtable :qt)
+
+;;;;Hello Qt
+
+(defun main ()
+    (let* ((app (make-qapplication))
+           (label (#_new QLabel "<strong>Hello Qt!</strong>")))
+        (#_setWindowTitle label "Hello, Qt!")
+        (#_resize label 100 100)
+        (#_show label)
+        (unwind-protect
+            (#_exec app)
+        (#_hide label))))
